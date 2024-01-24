a779f301a707a0864d7689131a59ed9258ed02c3
Ckala62rus <agr.akyla@mail.ru>
2024-01-24 22:46:25 +0300

    (fix) forgot add file with keyboard.

---
+++ kbds/reply.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+start_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Первая кнопка"),
+            KeyboardButton(text="Вторая кнопка"),
+        ],
+        [
+            KeyboardButton(text="Третья кнопка"),
+            KeyboardButton(text="Четвертая кнопка"),
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Что вас интересует?"
+)
