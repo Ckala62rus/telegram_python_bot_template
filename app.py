e8d9e78b1d3cfc6681405a5ce01e4dc751b2578a
Ckala62rus <agr.akyla@mail.ru>
2024-09-20 14:26:21 +0300

    (refactor) added logging app.py file.

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import asyncio
-import logging
+import logging.config
 import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
-from sheduler import scheduler_tasks
-
 from database.db import session_factory
-from handlers.user_private import user_private_router
 from common.bot_cmds_list import private
+from handlers.user_private import user_private_router
 from handlers.user_proup import user_group_router
 from handlers.admin_private import admin_router
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from sheduler import scheduler_tasks
 
-from utils.logger_project import my_logger
+from utils.logger_project import logging_config
+
+# Загружаем настройки логирования из словаря `logging_config`
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger(__name__)
+
 
 load_dotenv(find_dotenv())
 from middleware.db_middleware import DatabaseSessionMiddleware, \
@@ -35,16 +40,17 @@ ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
 
 async def main():
+    logger.debug('start application')
     # cron scheduler apscheduler
     # scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
     # scheduler.add_job(scheduler_tasks.every_minutes, trigger='interval', seconds=60, kwargs={'bot': bot})
     # scheduler.start()
 
     # init database session via middleware
-    my_logger.debug('*** init middleware')
+    logger.debug('init middlewares')
     dp.update.middleware(DatabaseSessionMiddleware(session_pool=session_factory))
     dp.update.middleware(SaveInputCommandMiddleware())
-    my_logger.debug('*** middleware was loaded')
+    logger.debug('end init middlewares')
 
     await bot.delete_webhook(drop_pending_updates=True)
     await bot.delete_my_commands(scope=BotCommandScopeAllPrivateChats())
@@ -52,6 +58,7 @@ async def main():
         commands=private,
         scope=BotCommandScopeAllPrivateChats()
     )
+    logger.debug('start polling')
     # await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
