19c482e76a8ddc270f039c375deabec3d2ccc0e9
Ckala62rus <agr.akyla@mail.ru>
2024-09-24 14:44:29 +0300

    (feature) add non require last btn and size for inline keyboard markup.

--- kbds/inline.py
+++ kbds/inline.py
@@ -5,12 +5,18 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_callback_btns(
         *,
         btns: dict[str, str],
-        size: tuple[int] = (2,)
+        size: tuple[int] = (2,),
+        btns_last: dict[str, str] = None,
+        size_last: tuple[int] = (1,)
 ):
     keyboard = InlineKeyboardBuilder()
 
     for text, data in btns.items():
         keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
+
+    if btns_last is not None and len(btns_last) > 0:
+        for key, value in btns_last.items():
+            keyboard.add(InlineKeyboardButton(text=key, callback_data=value))
 
     return keyboard.adjust(*size).as_markup()
 
